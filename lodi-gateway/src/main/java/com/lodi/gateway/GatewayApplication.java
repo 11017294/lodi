7fb2a44d8c9b91a404cdda18f854e284341980a0
maybeBin <11017294@qq.com>
2023-09-26 16:47:40 +0800

    feat(nacos-config):整合nacos-config

--- lodi-gateway/src/main/java/com/lodi/gateway/GatewayApplication.java
+++ lodi-gateway/src/main/java/com/lodi/gateway/GatewayApplication.java
@@ -1,14 +1,20 @@
 package com.lodi.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * 网关服务
  * @author MaybeBin
  * @createDate 2023-09-19
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
         System.out.println(
                 "(♥◠‿◠)ﾉﾞ  网关服务启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " _                   _   _      \n" +
