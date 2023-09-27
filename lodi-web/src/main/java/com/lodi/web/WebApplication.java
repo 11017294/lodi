3e166c58164490eea0c07e3b92c4d8faa4d03549
maybeBin <11017294@qq.com>
2023-09-27 11:30:04 +0800

    feat(lodi-web):初始化lodi-web

--- lodi-web/src/main/java/com/lodi/web/WebApplication.java
+++ lodi-web/src/main/java/com/lodi/web/WebApplication.java
@@ -2,6 +2,8 @@ package com.lodi.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * web 服务
@@ -9,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @createDate 2023-09-19
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {
+        "com.lodi.web",
+        "com.lodi.common.config"
+})
 public class WebApplication {
 
     public static void main(String[] args) {
