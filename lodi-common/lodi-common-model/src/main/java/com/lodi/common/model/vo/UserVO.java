39a18c71d0e21736f924b3b922fc186115dcedff
maybeBin <11017294@qq.com>
2024-04-14 22:25:05 +0800

    feat(用户视图):增加用户注册日期

--- lodi-common/lodi-common-model/src/main/java/com/lodi/common/model/vo/UserVO.java
+++ lodi-common/lodi-common-model/src/main/java/com/lodi/common/model/vo/UserVO.java
@@ -1,5 +1,6 @@
 package com.lodi.common.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -20,106 +21,60 @@ public class UserVO implements Serializable {
 
     private Long id;
 
-    /**
-     * 用户名
-     */
     @Schema(description = "用户名")
     private String username;
 
-    /**
-     * 昵称
-     */
     @Schema(description = "昵称")
     private String nickname;
 
-    /**
-     * 用户头像
-     */
     @Schema(description = "用户头像")
     private String userAvatar;
 
-    /**
-     * 自我简介最多150字
-     */
     @Schema(description = "自我简介最多", maxLength = 150)
     private String summary;
 
-    /**
-     * 出生年月日
-     */
     @Schema(description = "出生年月日")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
 
-    /**
-     * 性别：0-默认 1-男 2-女
-     */
-    @Schema(description = "性别")
+    @Schema(description = "性别（0-默认 1-男 2-女）")
     private Integer gender;
 
-    /**
-     * 邮箱
-     */
     @Schema(description = "邮箱")
     private String email;
 
-    /**
-     * 用户角色：user / admin
-     */
-    @Schema(description = "用户角色")
+    @Schema(description = "用户角色（user / admin）")
     private String userRole;
 
-    /**
-     * 登录次数
-     */
     @Schema(description = "登录次数")
     private Integer loginCount;
 
-    /**
-     * ip来源
-     */
     @Schema(description = "ip来源")
     private String ipSource;
 
-    /**
-     * 浏览器
-     */
     @Schema(description = "浏览器")
     private String browser;
 
-    /**
-     * 操作系统
-     */
     @Schema(description = "操作系统")
     private String os;
 
-    /**
-     * 最后登录时间
-     */
     @Schema(description = "最后登录时间")
     private Date lastLoginTime;
 
-    /**
-     * 最后登录IP
-     */
     @Schema(description = "最后登录IP")
     private String lastLoginIp;
 
-    /**
-     * 是否开启邮件通知：0-关闭 1-开启
-     */
-    @Schema(description = "是否开启邮件通知")
+    @Schema(description = "是否开启邮件通知（0-关闭 1-开启）")
     private Integer startEmailNotify;
 
-    /**
-     * 状态：0-禁用 1-正常
-     */
-    @Schema(description = "状态")
+    @Schema(description = "状态（0-禁用 1-正常）")
     private Integer status;
 
-    /**
-     * 评论状态：0-禁言 1-正常
-     */
-    @Schema(description = "评论状态")
+    @Schema(description = "评论状态（0-禁言 1-正常）")
     private Integer commentStatus;
+
+    @Schema(description = "注册日期")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date createTime;
 
 }
