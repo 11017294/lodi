5cc71513fd3954a52953c514fe0cbc905a7ce9c9
maybeBin <11017294@qq.com>
2024-05-14 11:12:07 +0800

    feat(cache):SpringCache 整合 Redis

--- lodi-common/lodi-common-redis/src/main/java/com/lodi/common/redis/RedisConfig.java
+++ lodi-common/lodi-common-redis/src/main/java/com/lodi/common/redis/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -31,22 +32,24 @@ public class RedisConfig extends CachingConfigurerSupport {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        serializer.setObjectMapper(mapper);
-
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
+        template.setValueSerializer(buildRedisSerializer());
 
         // Hash的key也采用StringRedisSerializer的序列化方式
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(serializer);
+        template.setHashValueSerializer(buildRedisSerializer());
 
         template.afterPropertiesSet();
         return template;
     }
 
+    public static RedisSerializer<?> buildRedisSerializer() {
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        return serializer;
+    }
 }
---
+++ lodi-common/lodi-common-redis/src/main/java/com/lodi/common/redis/TimeoutRedisCacheManager.java
@@ -0,0 +1,92 @@
+package com.lodi.common.redis;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.util.StringUtils;
+
+import java.time.Duration;
+
+import static com.lodi.common.redis.CacheAutoConfiguration.SEPARATOR;
+import static com.lodi.common.redis.CacheAutoConfiguration.SPLIT;
+
+/**
+ * 支持自定义过期时间的 {@link RedisCacheManager} 实现类
+ * <p>
+ * 在 {@link Cacheable#cacheNames()} 格式为 "key#ttl" 时，# 后面的 ttl 为过期时间
+ * 单位为最后一个字母（支持的单位有：d-天、h-小时、m-分钟、s-秒），默认单位为 s 秒
+ *
+ * @author MaybeBin
+ * @createDate 2024-05-08
+ */
+public class TimeoutRedisCacheManager extends RedisCacheManager {
+
+    public TimeoutRedisCacheManager(RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfiguration) {
+        super(cacheWriter, defaultCacheConfiguration);
+    }
+
+    @Override
+    protected RedisCache createRedisCache(String name, RedisCacheConfiguration cacheConfig) {
+        if (!StringUtils.hasText(name)) {
+            return super.createRedisCache(name, cacheConfig);
+        }
+        // 如果使用 # 分隔，大小不为 2，则说明不使用自定义过期时间
+        String[] names = StringUtils.split(name, SPLIT);
+        if (names.length != 2) {
+            return super.createRedisCache(name, cacheConfig);
+        }
+
+        // 核心：通过修改 cacheConfig 的过期时间，实现自定义过期时间
+        if (cacheConfig != null) {
+            // 移除 # 后面的 : 以及后面的内容，避免影响解析
+            int pos = names[1].indexOf(SEPARATOR);
+            if (-1 != pos) {
+                names[1] = names[1].substring(0, pos);
+            }
+            // 解析时间
+            Duration duration = parseDuration(names[1]);
+            cacheConfig = cacheConfig.entryTtl(duration);
+        }
+        return super.createRedisCache(name, cacheConfig);
+    }
+
+    /**
+     * 解析过期时间 Duration
+     *
+     * @param ttlStr 过期时间字符串
+     * @return 过期时间 Duration
+     */
+    private Duration parseDuration(String ttlStr) {
+        int index = ttlStr.length() - 1 > 0 ? ttlStr.length() - 1 : 0;
+        String timeUnit = ttlStr.substring(index);
+        String timeStr = ttlStr.substring(0, index);
+        switch (timeUnit) {
+            case "d":
+                return Duration.ofDays(removeDurationSuffix(timeStr));
+            case "h":
+                return Duration.ofHours(removeDurationSuffix(timeStr));
+            case "m":
+                return Duration.ofMinutes(removeDurationSuffix(timeStr));
+            case "s":
+                return Duration.ofSeconds(removeDurationSuffix(timeStr));
+            default:
+                return Duration.ofSeconds(Long.parseLong(timeStr));
+        }
+    }
+
+    /**
+     * 移除多余的后缀，返回具体的时间
+     *
+     * @param ttlStr 过期时间字符串
+     * @return 时间
+     */
+    private Long removeDurationSuffix(String ttlStr) {
+        if (!StringUtils.hasText(ttlStr)) {
+            return 0L;
+        }
+        return Long.parseLong(ttlStr);
+    }
+
+}
